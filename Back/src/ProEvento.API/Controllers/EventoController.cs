27fbb84b3feefd48d6b0cd2a65ec5e247605b68f
aluisiojuniorr <aluisiojuniorr@gmail.com>
2022-09-11 09:08:14 -0300

    commit 1

---
+++ Back/src/ProEvento.API/Controllers/EventoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEvento.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEvento.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento []
+            { 
+                new Evento(){ },
+                new Evento(){ },
+            };
+        }
+    }
+}
---
+++ Back/src/ProEvento.API/Models/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEvento.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public int DataEvento { get; set; }
+        public int Tema { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+        public int ImagemURL { get; set; }
+    }
+}
